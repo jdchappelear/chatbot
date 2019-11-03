7db4751a05ad80f66d7da53894c8fb707c2b451b
John Chappelear <john.chappelear@gmail.com>
2019-11-02 20:34:13 -0400

    filled in most stuff from tutorial

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../chat.service';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/scan';
 
 @Component({
   selector: 'app-chat-dialog',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatDialogComponent implements OnInit {
 
-  constructor() { }
+  messages: Observable<Message[]>;
+  formValue: string;
+
+  constructor(public chatService: ChatService) { }
 
   ngOnInit() {
+    // appends to array after each new message is added to feedSource
+    this.messages = this.chatService.conversation.asObservable()
+        .scan((acc, val) => acc.concat(val) );
+  }
+
+  sendMessage() {
+    this.chatService.converse(this.formValue);
+    this.formValue = '';
   }
 
 }
